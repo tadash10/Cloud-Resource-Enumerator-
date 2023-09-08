e292f0fb129d2bcb5b1dbcb99200f2bd504e5723
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-08 23:34:15 +0000

    Create file_operations.py

---
+++ file_operations.py
@@ -0,0 +1,9 @@
+# file_operations.py
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        return file.read()
+
+def write_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
